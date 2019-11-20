184da5d3058c0987c69eed8bbad709bd7855de62
eliseverschelde <eliseverschelde@w210h251.wireless.ugent.be>
2019-11-20 16:10:09 +0100

    grote kuis

--- src/project/FXMLDocumentController.java
+++ src/project/FXMLDocumentController.java
@@ -10,26 +10,35 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import project.LOGIC.DomeinController;
 
 /**
+ * FXML Controller class
  *
- * @author astridvermeulen
+ * @author eliseverschelde
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    @FXML
+    private Button button;
     @FXML
     private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
+    private DomeinController dc;
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        dc = DomeinController.getInstance();
     }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        label.setText(dc.toonVluchtNaam());
+    }
     
 }
