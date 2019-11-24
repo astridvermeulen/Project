51bbdbdb55aae648b6591a0724e0fc8e5c3639e6
klaas <klaas@172.17.254.182>
2019-11-24 15:18:05 +0100

    retourneren arraylisten toegevoegd

--- src/project/DB/DBBooking.java
+++ src/project/DB/DBBooking.java
@@ -46,7 +46,8 @@ public class DBBooking {
       con = DBConnector.getConnection();
       Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
       
-      String sql = "SELECT bookingNumber, serviceFee, promotion, flightNumber, departureDate "
+      String sql = "SELECT bookingNumber "
+        //+ ", promotion, serviceFee, flightNumber, departureDate "
         + "FROM db2019_18.booking "
 	+ "WHERE bookingNumber = " + bookingNumber;
 
@@ -58,20 +59,20 @@ public class DBBooking {
       
       if (srs.next()) {
           bookingNumber = srs.getInt("bookingNumber");
-          serviceFee = srs.getDouble("serviceFee");
-          promotion = srs.getDouble("promotion");
-          flightNumber = srs.getString("flightNumber");
-          departureDate = srs.getDate("departureDate");
+          //serviceFee = srs.getDouble("serviceFee");
+          //promotion = srs.getDouble("promotion");
+          //flightNumber = srs.getString("flightNumber");
+          //departureDate = srs.getDate("departureDate");
 	} 
       else {// we verwachten slechts 1 rij...
 	DBConnector.closeConnection(con);
 	return null;
       }
       
-      //Booking boeking = new Booking(bookingNumber, serviceFee, promotion, flightNumber, departureDate);
+      Booking boeking = new Booking(bookingNumber);
               DBConnector.closeConnection(con);
-      //return boeking;
-      return null;
+      return boeking;
+      
     }
     
     catch (Exception ex) {
