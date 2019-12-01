a483c5500a9c415668a086eeb7a2c3c3f5b956af
astridvermeulen <astridvermeulen@w199h211.wireless.ugent.be>
2019-12-01 13:53:53 +0100

    ly all

--- src/project/LOGIC/DomainController.java
+++ src/project/LOGIC/DomainController.java
@@ -1,5 +1,6 @@
 package project.LOGIC;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import project.DB.DBAirport;
@@ -19,9 +20,11 @@ public class DomainController {
     }
 
     //Method to filter the flights, returns the filtered flights 
-    public ArrayList<Flight> searchFlight(Boolean legs, String filter) throws DBException {
+    public ArrayList<Flight> searchFlight(Boolean legs, String filter, String origin, String destination, String departureDate) throws DBException {
         ArrayList<Flight> flightsAll = DBFlight.getFlights();
-        ArrayList<Flight> flightsFilteredOnLegs = this.fliterOnLegs(legs, flightsAll);
+        ArrayList<Flight> flightsOriginDestination = this.fliterOnOriginDestination(origin, destination, flightsAll);
+         ArrayList<Flight> flightsDate = this.filterOnDate(departureDate,flightsOriginDestination);
+        ArrayList<Flight> flightsFilteredOnLegs = this.fliterOnLegs(legs, flightsDate);
         MergeSort.sort(flightsFilteredOnLegs, filter);
         return flightsFilteredOnLegs;
     }
@@ -41,23 +44,44 @@ public class DomainController {
         return flightsFiltered;
     }
 
+    private ArrayList<Flight> fliterOnOriginDestination(String origin, String destination, ArrayList<Flight> flightsAll) {
+        ArrayList<Flight> flightsOriginDestination = new ArrayList<>();
+        for (Flight vlucht : flightsAll) {
+            if (vlucht.getOrigin().equalsIgnoreCase(origin) && vlucht.getDestination().equalsIgnoreCase(destination)) {
+                flightsOriginDestination.add(vlucht);
+            }
+        }
+        return flightsOriginDestination;
+    }
+    
+    private ArrayList<Flight> filterOnDate(String departureDate, ArrayList<Flight> flightsOriginDestination){
+        ArrayList<Flight> flightsDate = new ArrayList<>();
+        LocalDate departureD = LocalDate.parse(departureDate);
+        for (Flight vlucht : flightsOriginDestination) {
+            if (vlucht.getDepartureDate().equals(departureD) ) {
+                flightsDate.add(vlucht);
+            }
+        }
+        return flightsDate;
+    }
+
     public ArrayList<Customer> customersOverview() throws DBException {
         ArrayList<Customer> customersAll = DBCustomer.getCustomers();
         return customersAll;
     }
 
-    public ArrayList<String> airportsAlphabetic() throws DBException{
+    public ArrayList<String> airportsAlphabetic() throws DBException {
         ArrayList<Airport> airportsAll = DBAirport.getAirports();
         ArrayList<String> airportNames = new ArrayList();
-        for(int i = 0; i < airportsAll.size(); i++){
-            airportNames.set(i, airportsAll.get(i).getAirportName()); }
+        for (int i = 0; i < airportsAll.size(); i++) {
+            airportNames.set(i, airportsAll.get(i).getAirportName());
+        }
         Collections.sort(airportNames);
         return airportNames;
     }
-    
-    
+
     public static void main(String[] args) {
-        
+
     }
 
 }
